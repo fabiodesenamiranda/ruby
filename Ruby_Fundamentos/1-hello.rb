bdce5aceac9494320d11ded053a5a221542b96e2
Fabio de Sena Miranda <fabiodesenamiranda@hotmail.com>
2024-03-09 18:13:13 -0300

    fundamentos_ruby

---
+++ Ruby_Fundamentos/1-hello.rb
@@ -0,0 +1,15 @@
+#Comandos de Saida de Dados
+
+=begin
+    Estou aprendendo
+    a linguagem Ruby
+    na One Beat Code
+
+=end
+
+print "Hello World\n" 
+print "Ola Mundo\n"
+
+puts "Hello World"
+puts "Ola Mundo"
+
---
+++ Ruby_Fundamentos/2-tipos.rb
@@ -0,0 +1,16 @@
+#Dados do Filme
+
+name = "Avatar"
+yearLaunch = 2022
+price = 100.00
+planIncluded = false
+
+
+puts name
+
+#identificando tipos de dados
+puts name.class
+puts yearLaunch.class
+puts price.class
+puts planIncluded.class
+
